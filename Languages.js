81d9568511e36ea9676d4fe81ff0920de5d3d97f
abhir <abhiramashok1062004@gmail.com>
2024-07-02 13:24:16 +0530

    Initial Commit

--- Languages.js
+++ Languages.js
@@ -2,7 +2,6 @@ let languagesTools=document.getElementsByClassName('languagesTools');
 let blackBox=document.getElementById('blackBox');
 for(let i=0;i<languagesTools.length;i++){
     let height=languagesTools[i].offsetHeight;
-    console.log(height, window.innerHeight);
 }
 
 let ProjectName=document.getElementById('ExpenseTracker');
@@ -19,13 +18,17 @@ function displayTools(projectName){
         ProjectName.style.top=((window.innerHeight/2)-(height/2))+'px';
         blackBox.style.height=(document.body.offsetHeight+70)+'px';
     });
+    blackBox.style.opacity = '0.85';
     ProjectName.style.transform = 'scale(1)';
 }
 
 blackBox.addEventListener('click',function(){
-    blackBox.style.display = 'none';
     ProjectName.style.transform = 'scale(0)';
-    ProjectName.style.display = 'none';
+    blackBox.style.opacity = '0';
+    setTimeout(() => {
+        ProjectName.style.display = 'none';
+        blackBox.style.display = 'none';
+    }, 350);
 });
 
 
