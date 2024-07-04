90a9263d5690a6d32f7b423dc9f30e551e63fcae
abhir <abhiramashok1062004@gmail.com>
2024-07-04 21:15:20 +0530

    Initial Commit

--- Languages.js
+++ Languages.js
@@ -4,12 +4,12 @@ for(let i=0;i<languagesTools.length;i++){
     let height=languagesTools[i].offsetHeight;
 }
 
-let ProjectName=document.getElementById('ExpenseTracker');
+let ProjectName=document.getElementById('languagesTools');
 function displayTools(projectName){
-    ProjectName=document.getElementById(projectName);
     blackBox.style.height=document.body.clientHeight+'px';
     blackBox.style.display = 'block';
     ProjectName.style.display = 'block';
+    createLangIcon(projectName);
     blackBox.style.height=(document.body.offsetHeight+70)+'px';
     let height=parseFloat(window.getComputedStyle(ProjectName).height);
     ProjectName.style.top=((window.innerHeight/2)-(height/2))+'px';
@@ -45,5 +45,37 @@ function languageToolsDark(){
     for(let i=0;i<langIconText.length;i++){
         langIconText[i].style.color = 'white';
         langIconText[i].style.opacity = '0.8';
+    }
+}
+
+
+//Updating Languages & Tools
+let langIconContainer=document.getElementById('langIconContainer');
+let ExpenseTracker = ['EXPENSE TRACKER', 'expense', 'html', 'css', 'JavaScript'];
+let UnitConverter = ['UNIT CONVERTER', 'libra', 'html', 'css', 'JavaScript'];
+let GregorianCalendar = ['GREGORIAN CALENDAR', 'calendar', 'html', 'css', 'JavaScript'];
+
+function createLangIcon(projectName){
+    langIconContainer.innerHTML='';
+    if(projectName==='ExpenseTracker'){
+        appendIcon(ExpenseTracker);
+    }
+    else if(projectName==='UnitConverter'){
+        appendIcon(UnitConverter);
+    }
+    else if(projectName==='GregorianCalendar'){
+        appendIcon(GregorianCalendar);
+    }
+}
+function appendIcon(projectNameVariable){
+    let originalImg=document.getElementById(projectNameVariable[1]);
+    let clonedImg=originalImg.cloneNode(true);
+    document.getElementById('imgContainer').innerHTML='';
+    document.getElementById('imgContainer').appendChild(clonedImg);
+    document.getElementById('ProjectIdentifier').textContent=projectNameVariable[0];
+    for(let i=2;i<projectNameVariable.length;i++){
+        let originalIcon=document.getElementById(projectNameVariable[i]);
+        let clonedIcon=originalIcon.cloneNode(true);
+        langIconContainer.appendChild(clonedIcon);
     }
 }
